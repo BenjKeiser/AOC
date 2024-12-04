c80e893e2f166dce24bb36dd404099d49e8c705d
Benjamin Keiser <benjkeiser@gmail.com>
2024-12-04 11:36:00 +0100

    day4 part1

---
+++ 2024/day4/src/main.rs
@@ -0,0 +1,98 @@
+use std::fs;
+use std::fmt;
+use std::error::Error;
+use std::ffi::OsString;
+use std::env;
+use std::collections::VecDeque;
+use std::slice::Iter;
+
+pub struct Direction {
+    x: i32,
+    y: i32,
+}
+
+impl Direction {
+    pub fn iterator() -> Iter<'static, Direction> {
+        static DIRECTIONS: [Direction; 8] = [Direction { x: -1, y: -1}, 
+                                             Direction { x: 0, y: -1}, 
+                                             Direction { x: 1, y: -1}, 
+                                             Direction { x: 1, y: 0}, 
+                                             Direction { x: 1, y: 1}, 
+                                             Direction { x: 0, y: 1}, 
+                                             Direction { x: -1, y: 1}, 
+                                             Direction { x: -1, y: 0}];
+        DIRECTIONS.iter()
+    }
+}
+
+impl fmt::Display for Direction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
+}
+
+fn find_next_char(c_list: &VecDeque<char>, map: &Vec<Vec<char>>, x: usize, y: usize, dir: &Direction) -> bool {
+    let mut ret = false;
+    if c_list.len() != 0 {
+        let c = c_list[0];
+        let mut next = c_list.clone();
+        next.pop_front();
+
+        let x_coord = (x as i32) + dir.x;
+        let y_coord = (y as i32) + dir.y;
+
+        if x_coord >= 0 && y_coord >= 0 {
+            let x_coord = x_coord as usize;
+            let y_coord = y_coord as usize;
+            if x_coord < map[0].len() && y_coord < map.len() {
+                if map[y_coord][x_coord] == c {
+                    ret = find_next_char(&next, map, x_coord, y_coord, dir);
+                }
+            }
+        }  
+    }
+    else {
+        ret = true;
+    }
+
+    ret
+}
+
+fn find_words(word: &str, map: &Vec<Vec<char>>) -> i32 {
+    let mut w_vec: VecDeque<char> = word.chars().collect();
+    let mut sum = 0;
+    let c = w_vec[0];
+    
+
+    w_vec.pop_front();
+    //find_next_char(&w_vec, map, 5, 0, &Direction {x: 1, y: 0});
+    for i in 0..map.len() {
+        for pos in map[i].iter().enumerate().filter(|(_, x)| **x == c).map(|(idx, _)| idx) {
+            for dir in Direction::iterator() {
+                if find_next_char(&w_vec, map, pos, i, dir) {
+                    sum += 1;
+                }
+            }
+        }
+    }
+    sum
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file_path = get_first_arg()?;
+    let mut word_map: Vec<Vec<char>> = Vec::new();
+    for line in fs::read_to_string(file_path)?.lines(){
+        let char_vec: Vec<char> = line.chars().collect();
+        word_map.push(char_vec);
+    }
+    let words = find_words("XMAS", &word_map);
+    println!("part1: {words}");
+    Ok(())
+}
