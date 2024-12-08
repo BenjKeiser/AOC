b480ca96bc51506b74a76ec99675c2e024ada528
Benjamin Keiser <benjkeiser@gmail.com>
2024-12-08 20:44:40 +0100

    day8 part2

--- 2024/day8/src/main.rs
+++ 2024/day8/src/main.rs
@@ -49,7 +49,7 @@ fn get_antinodes(antennas: &HashMap<char, Vec<(usize, usize)>>, y_max: usize, x_
     for key in antennas.keys() {
         if let Some(a_list) = antennas.get(key) {
             for i in 0..a_list.len() {
-                for z in i+1..a_list.len() {
+                for z in i + 1..a_list.len() {
                     let pos = calc_pos(a_list[i], a_list[z], y_max, x_max);
                     for p in pos {
                         if !a_nodes.contains(&p) {
@@ -58,6 +58,87 @@ fn get_antinodes(antennas: &HashMap<char, Vec<(usize, usize)>>, y_max: usize, x_
                             //println!("Added Antinode: {:?},{:?} -> {:?}", a_list[i], a_list[z], p);
                         }
                     }
+                }
+            }
+        }
+    }
+
+    nb_nodes
+}
+
+fn calc_pos_resonance(
+    a: (usize, usize),
+    b: (usize, usize),
+    y_max: usize,
+    x_max: usize,
+) -> Vec<(usize, usize)> {
+    let mut positions: Vec<(usize, usize)> = Vec::new();
+
+    let y_diff: i32 = a.0 as i32 - b.0 as i32;
+    let x_diff: i32 = a.1 as i32 - b.1 as i32;
+
+    //first position
+    let mut p_y = a.0 as i32;
+    let mut p_x = a.1 as i32;
+
+    loop {
+        p_y = p_y + y_diff;
+        p_x = p_x + x_diff;
+
+        if p_y >= 0 && p_y <= y_max as i32 && p_x >= 0 && p_x <= x_max as i32 {
+            positions.push((p_y as usize, p_x as usize));
+        } else {
+            break;
+        }
+    }
+
+    //second position
+    p_y = b.0 as i32;
+    p_x = b.1 as i32;
+
+    loop {
+        p_y = p_y - y_diff;
+        p_x = p_x - x_diff;
+
+        if p_y >= 0 && p_y <= y_max as i32 && p_x >= 0 && p_x <= x_max as i32 {
+            positions.push((p_y as usize, p_x as usize));
+        } else {
+            break;
+        }
+    }
+
+    positions
+}
+
+fn get_antinodes_resonance(
+    antennas: &HashMap<char, Vec<(usize, usize)>>,
+    y_max: usize,
+    x_max: usize,
+) -> u64 {
+    let mut nb_nodes = 0;
+    let mut a_nodes: Vec<(usize, usize)> = Vec::new();
+    //println!("Antennas: {:?}", antennas);
+
+    for key in antennas.keys() {
+        if let Some(a_list) = antennas.get(key) {
+            for i in 0..a_list.len() {
+                for z in i+1..a_list.len() {
+                    let pos = calc_pos_resonance(a_list[i], a_list[z], y_max, x_max);
+                    for p in pos {
+                        if !a_nodes.contains(&p) {
+                            a_nodes.push(p);
+                            nb_nodes += 1;
+                            //println!("Added Antinode: {:?},{:?} -> {:?}", a_list[i], a_list[z], p);
+                        }
+                    }
+                }
+            }
+            //each antenna position can be an antinode
+            for p in a_list{
+                if !a_nodes.contains(p) {
+                    a_nodes.push(*p);
+                    nb_nodes += 1;
+                    //println!("Added Antinode: {:?}", p);
                 }
             }
         }
@@ -91,8 +172,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     let a_nodes = get_antinodes(&antennas, map.len() - 1, map[0].len() - 1);
-
     println!("part1: {a_nodes}");
+    let a_nodes = get_antinodes_resonance(&antennas, map.len() - 1, map[0].len() - 1);
+    println!("part2: {a_nodes}");
     Ok(())
 }
 //1711 for part2
