cceaa69ff9c6eb0fbc169ddb34639b081923f6e2
Benjamin Keiser <benjkeiser@gmail.com>
2024-12-22 14:06:31 +0100

    day22 part2

--- 2024/day22/src/main.rs
+++ 2024/day22/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::env;
 use std::error::Error;
 use std::ffi::OsString;
@@ -12,18 +13,18 @@ fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
 }
 
 fn mix_nb(nb: usize, nb2: usize) -> usize {
-    return nb ^ nb2
+    return nb ^ nb2;
 }
 
 fn prune_nb(nb: usize) -> usize {
-    return nb % 16777216
+    return nb % 16777216;
 }
 
 fn get_next_secret_nb(nb: usize) -> usize {
     let mut secret = nb;
-    secret = prune_nb(mix_nb(secret, secret*64));
-    secret = prune_nb(mix_nb(secret, secret/32));
-    secret = prune_nb(mix_nb(secret, secret*2048));
+    secret = prune_nb(mix_nb(secret, secret * 64));
+    secret = prune_nb(mix_nb(secret, secret / 32));
+    secret = prune_nb(mix_nb(secret, secret * 2048));
 
     secret
 }
@@ -42,6 +43,53 @@ fn get_secret_nb_sum(secrets: &Vec<usize>, iterations: usize) -> usize {
     sum
 }
 
+fn get_most_bananas(secrets: &Vec<usize>, iterations: usize) -> usize {
+    let mut bananas = 0;
+    let mut value_map: Vec<Vec<usize>> = vec![Vec::new(); 19_usize.pow(4)];
+    for s in secrets {
+        let mut secret = *s;
+        let mut trade_values: Vec<usize> = Vec::new();
+        let mut idxs = HashSet::new();
+        trade_values.push(secret % 10);
+        //println!("{secret} => {}", (secret % 10) as u8);
+        for _ in 0..iterations {
+            secret = get_next_secret_nb(secret);
+            trade_values.push(secret % 10);
+            //println!("{secret} => {}", (secret % 10) as u8);
+        }
+        //println!("{:?}", trade_values);
+        for i in 4..trade_values.len() {
+            let x = ((trade_values[i - 3] as i8 - trade_values[i - 4] as i8) + 9) as usize;
+            let y = ((trade_values[i - 2] as i8 - trade_values[i - 3] as i8) + 9) as usize;
+            let z = ((trade_values[i - 1] as i8 - trade_values[i - 2] as i8) + 9) as usize;
+            let k = ((trade_values[i] as i8 - trade_values[i - 1] as i8) + 9) as usize;
+            /*
+            println!(
+                "{}, {}, {}, {} => {}",
+                x as i8 - 9,
+                y as i8 - 9,
+                z as i8 - 9,
+                k as i8 - 9,
+                trade_values[i]
+            );
+            */
+            let index = x * 19_usize.pow(3) + y * 19_usize.pow(2) + z * 19 + k;
+            if !idxs.contains(&index) {
+                value_map[index].push(trade_values[i]);
+                idxs.insert(index);
+            }
+        }
+    }
+
+    value_map.iter().for_each(|x| {
+        let b: usize = x.iter().sum();
+        if b > bananas {
+            bananas = b;
+        }
+    });
+
+    bananas
+}
 fn main() -> Result<(), Box<dyn Error>> {
     let file_path = get_first_arg()?;
 
@@ -63,10 +111,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     );
 
     let start = Instant::now();
-    let secret_sum = 0;
+    let bananas = get_most_bananas(&vendors, 2000);
     let duration = start.elapsed();
     println!(
-        "Part2: {secret_sum} | {}s {}ms {}µs {}ns",
+        "Part2: {bananas} | {}s {}ms {}µs {}ns",
         duration.as_secs(),
         duration.subsec_millis(),
         duration.subsec_micros() % 1000,
