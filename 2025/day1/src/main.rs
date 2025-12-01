2f0b59f8c93bea8731c1e50049936495a8975ec5
BenjKeiser <benjkeiser@gmail.com>
2025-12-01 15:17:31 +0000

    added structure for AoC2025

---
+++ 2025/day1/src/main.rs
@@ -0,0 +1,13 @@
+
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file_path = get_first_arg()?;
+    
+    Ok(())
+}
