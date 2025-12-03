7d67210bda7835ed95eef79110d691a58a0940ea
Benjamin Keiser <benjkeiser@gmail.com>
2025-12-03 10:52:04 +0100

    day2 part2

--- 2025/day3/src/main.rs
+++ 2025/day3/src/main.rs
@@ -11,11 +11,13 @@ fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
 }
 
 fn get_joltage(bank: &str) -> Result<u32, Box<dyn Error>> {
-    if let Some((pos, first)) = bank.chars().enumerate().take(bank.chars().count() - 1)
-        .max_by(|(p1, c1), (p2, c2)| {
-            c1.cmp(c2).then(p2.cmp(p1))
-        }) {
-        if let Some(rest) = bank.get(pos+1..) {
+    if let Some((pos, first)) = bank
+        .chars()
+        .enumerate()
+        .take(bank.chars().count() - 1)
+        .max_by(|(p1, c1), (p2, c2)| c1.cmp(c2).then(p2.cmp(p1)))
+    {
+        if let Some(rest) = bank.get(pos + 1..) {
             if let Some(second) = rest.chars().max_by_key(|&c| c) {
                 let s = format!("{}{}", first, second);
                 let joltage = s.parse::<u32>()?;
@@ -25,6 +27,44 @@ fn get_joltage(bank: &str) -> Result<u32, Box<dyn Error>> {
         }
     }
     Err("No valid battery found".into())
+}
+
+fn get_joltage_recursive(bank: &str, depth: usize) -> Option<u64> {
+    if bank.chars().count() <= depth {
+        return None;
+    }
+
+    if let Some(max) = bank
+        .chars()
+        .take(bank.chars().count() - depth)
+        .max_by_key(|&c| c)
+    {
+        if depth == 0 {
+            if let Some(val) = max.to_digit(10) {
+                return Some(val as u64);
+            }
+        }
+
+        let mut joltage = 0;
+        for (p, _) in bank.chars().enumerate().filter(|&(_, c)| c == max) {
+            if (bank.chars().count() - p) >= (depth - 1) {
+                if let Some(rest) = bank.get(p + 1..) {
+                    if let Some(j) = get_joltage_recursive(rest, depth - 1) {
+                        if j > joltage {
+                            joltage = j;
+                        }
+                    }
+                }
+            }
+        }
+        if joltage > 0 {
+            let s = format!("{}{}", max, joltage);
+            joltage = s.parse::<u64>().unwrap();
+            return Some(joltage);
+        }
+    }
+
+    None
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -40,6 +80,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let start = Instant::now();
 
     let total_joltage: u32 = banks.iter().filter_map(|b| get_joltage(b).ok()).sum();
+
+    let duration = start.elapsed();
+    println!(
+        "Part1: {} | {}s {}ms {}µs {}ns",
+        total_joltage,
+        duration.as_secs(),
+        duration.subsec_millis(),
+        duration.subsec_micros() % 1000,
+        duration.subsec_nanos() % 1000
+    );
+
+    let start = Instant::now();
+
+    let total_joltage: u64 = banks
+        .iter()
+        .filter_map(|b| get_joltage_recursive(b, 12 - 1))
+        .sum();
 
     let duration = start.elapsed();
     println!(
