151624fa5c08b4d317ccb74c29baaa4e22d494d9
Benjamin Keiser <benjkeiser@gmail.com>
2025-12-06 20:01:59 +0100

    day6 part2

--- 2025/day6/src/main.rs
+++ 2025/day6/src/main.rs
@@ -18,7 +18,7 @@ fn do_math(numbers: &Vec<Vec<i64>>, operators: &Vec<char>) -> i64 {
     let mut sum = 0;
     for (col, op) in operators.iter().enumerate() {
         let mut result = numbers[0][col];
-        for (_, nb) in numbers.iter().enumerate().skip(1) {
+        for nb in numbers.iter().skip(1) {
             match op {
                 '+' => {
                     result += nb[col];
@@ -37,11 +37,40 @@ fn do_math(numbers: &Vec<Vec<i64>>, operators: &Vec<char>) -> i64 {
     sum
 }
 
+fn do_ceph_math(numbers: &Vec<Vec<i64>>, operators: &Vec<char>) -> i64 {
+    let mut sum = 0;
+    for (idx, op) in operators.iter().enumerate() {
+        let mut result = numbers[idx][0];
+        for nb in numbers[idx].iter().skip(1) {
+            match op {
+                '+' => {
+                    result += *nb;
+                }
+                '*' => {
+                    result *= *nb;
+                }
+                _ => {
+                    result = 0;
+                }
+            }
+        }
+        sum += result;
+    }
+
+    sum
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let file_path = get_first_arg()?;
 
+    let mut input = Vec::new();
+    let mut ceph_numbers = Vec::new();
     let mut numbers: Vec<Vec<i64>> = Vec::new();
     let mut operators: Vec<char> = Vec::new();
+
+    
+    let start = Instant::now();
+
     for line in fs::read_to_string(file_path)?.lines() {
         if starts_with_number(line) {
             let nb: Vec<i64> = line
@@ -49,20 +78,51 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .map(|s| i64::from_str_radix(s, 10).unwrap())
                 .collect();
             numbers.push(nb);
+            input.push(line);
         } else {
             operators = line
                 .split_whitespace()
                 .map(|s| s.chars().next().unwrap())
                 .collect();
+
+            //parse input for part 2
+            let mut last = 0;
+            let mut ops: String = line.to_string();
+            ops.push('e');
+            for (p, _c) in ops.chars().skip(1).enumerate().filter(|(_p, c)| *c != ' ') {
+                let mut cn = Vec::new();
+                for idx in last..=p {
+                    let mut val = String::new();
+                    for row in 0..input.len() {
+                        let c = input[row].chars().nth(idx).unwrap();
+                        if c != ' ' {
+                            val.push(c);
+                        }
+                    }
+                    if starts_with_number(&val) {
+                        cn.push(val.parse::<i64>()?);
+                    }
+                }
+                ceph_numbers.push(cn);
+                last = p
+            }
         }
     }
-
-    //println!("{:?}", numbers);
-    //println!("{:?}", operators);
 
     if numbers[0].len() != operators.len() {
         return Err("MISMATCH".into());
     }
+
+    
+    let duration = start.elapsed();
+
+    println!(
+        "Parse: {}s {}ms {}µs {}ns",
+        duration.as_secs(),
+        duration.subsec_millis(),
+        duration.subsec_micros() % 1000,
+        duration.subsec_nanos() % 1000
+    );
 
     let start = Instant::now();
 
@@ -80,10 +140,12 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let start = Instant::now();
 
+    let sum = do_ceph_math(&ceph_numbers, &operators);
+
     let duration = start.elapsed();
     println!(
         "Part2: {} | {}s {}ms {}µs {}ns",
-        2,
+        sum,
         duration.as_secs(),
         duration.subsec_millis(),
         duration.subsec_micros() % 1000,
